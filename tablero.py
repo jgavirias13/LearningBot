015fb299475a6b83df79d8314dc32dd50e0afa55
jgaviri6 <jgavirias13@gmail.com>
2015-04-29 03:41:49 +0000

    Se agrega tablero con instancias de objeto

--- tablero.py
+++ tablero.py
@@ -4,47 +4,40 @@ import fichas
 
 class Tablero(object):
     tablero = []
-    def __init__(self):
-        peonB = u'\u2659'
-        peonN = u'\u265F'
-        torreB = u'\u2656'
-        torreN = u'\u265C'
-        caballoB = u'\u2658'
-        caballoN = u'\u265E'
-        alfilB = u'\u2657'
-        alfilN = u'\u265D'
-        reyB = u'\u2654'
-        reyN = u'\u265A'
-        reinaB = u'\u2655'
-        reinaN = u'\u265B'
-        self.tablero.append(torreN)
-        self.tablero.append(caballoN)
-        self.tablero.append(alfilN)
-        self.tablero.append(reinaN)
-        self.tablero.append(reyN)
-        self.tablero.append(alfilN)
-        self.tablero.append(caballoN)
-        self.tablero.append(torreN)
-        for i in range(8):
-            self.tablero.append(peonN)
-        for i in range(32):
-            self.tablero.append("-")
-        for i in range (8):
-            self.tablero.append(peonB)
-        self.tablero.append(torreB)
-        self.tablero.append(caballoB)
-        self.tablero.append(alfilB)
-        self.tablero.append(reinaB)
-        self.tablero.append(reyB)
-        self.tablero.append(alfilB)
-        self.tablero.append(caballoB)
-        self.tablero.append(torreB)
-
-columnas = "a b c d e f g h".split(" ")
-
+    def __init__(self, tab):
+        for i in tab:
+            switch(tab[i])
+                case '-1':   #Casilla blanca
+                    self.tablero.append(casilla("blanco"))
+                case '0':   #Casilla negra
+                    self.tablero.append(casilla("negro"))
+                case '1':   #Peon blanco
+                    self.tablero.append(peon(i,"blanco")
+                case '2':   #Torre blanca
+                    self.tablero.append(torre("blanco"))
+                case '3':   #Alfil blanco
+                    self.tablero.append(alfil("blanco"))
+                case '4':   #Caballo blanco
+                    self.tablero.append(caballo("blanco"))
+                case '5':   #Reina blanca
+                    self.tablero.append(reina("blanco"))
+                case '6':   #Rey blanco
+                    self.tablero.append(rey("blanco"))
+                case '7':   #Peon negro
+                    self.tablero.append(peon(i,"negro"))
+                case '8':   #Torre negra
+                    self.tablero.append(torre("negro"))
+                case '9':   #Alfil negro
+                    self.tablero.append(alfil("negro"))
+                case '10':   #Caballo negro
+                    self.tablero.append(caballo("negro"))
+                case '11':  #Reina negra
+                    self.tablero.append(reina("negro"))
+                case '12':
+                    self.tablero.append(rey("negro"))
 
 m = Tablero()
 for i in range (8):
     for j in range (8):
-        print m.tablero[i*8 + j],
+        print m.tablero[i*8 + j].tostring,
     print
