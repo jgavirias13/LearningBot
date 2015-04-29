567fbd26213b041d92b7823cb022800a57031e02
jalvar53 <jalvar53@eafit.edu.co>
2015-04-29 03:21:24 +0000

    Fichas con Unicode

--- fichas.py
+++ fichas.py
@@ -5,34 +5,55 @@ class peon(object):
     color = ""
     def __init__(self, pos, color):
         self.color = color
-        if(pos == 1 or pos == 6):
+        if((pos >= 8 and pos <= 15) or (pos >= 48 and pos <= 55)):
             movimientos.append(2)
     def tostring():
         if (self.color == "Blanco"):
             return u'\u2659'
         else:
             return u'\u265F'
+
 class torre(object):
     movimientos = [1,2,3,4,5,6,7,8]
     direccion =[0,2,4,6]
     def tostring():
-        return "Torre"
+        if (self.color == "Blanco"):
+            return u'\u2656'
+        else:
+            return u'\u265C'
+
 class caballo(object):
     direccion = [0,2,4,6]
     def tostring():
-        return "Caballo"
+        if (self.color == "Blanco"):
+            return u'\u2658'
+        else:
+            return u'\u265E'
+
 class alfil(object):
     movimientos = [1,2,3,4,5,6,7,8]
     direccion = [1,3,5,7]
     def tostring():
-        return "Alfil"
+        if (self.color == "Blanco"):
+            return u'\u2657'
+        else:
+            return u'\u265D'
+
 class rey(object):
     movimientos = [1]
     direccion = [0,1,2,3,4,5,6,7]
     def tostring():
-        return "Rey"
+        if (self.color == "Blanco"):
+            return u'\u2654'
+        else:
+            return u'\u265A'
+
 class reina(object):
     movimientos = [1,2,3,4,5,6,7,8]
     direccion = [0,1,2,3,4,5,6,7]
     def tostring():
-        return "Reina"
+        if (self.color == "Blanco"):
+            return u'\u2655'
+        else:
+            return u'\u265B'
+
