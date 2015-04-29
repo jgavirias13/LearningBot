00956981869aed708e9fa9327899d7d760517b0e
jgaviri6 <jgavirias13@gmail.com>
2015-04-29 04:20:16 +0000

    Correccion tablero con objetos

--- tablero.py
+++ tablero.py
@@ -5,38 +5,53 @@ import fichas
 class Tablero(object):
     tablero = []
     def __init__(self, tab):
+        options = {'b': casillaBlanca,
+                    'B': casillaNegra,
+                    'p': peonBlanco,
+                    'P': peonNegro,
+                    't': torreBlanca,
+                    'T': torreNegra,
+                    'a': alfilBlanco,
+                    'A': alfilNegro,
+                    'c': caballoBlanco,
+                    'C': caballoNegro,
+                    'r': reinaBlanca,
+                    'R': reinaNegra,
+                    'k': reyBlanco,
+                    'K': reyNegro,
+        }
+        def casillaBlanca():
+            self.tablero.append(casilla("blanco"))
+        def casillaNegra():
+            self.tablero.append(casilla("negro"))
+        def peonBlanco():
+            self.tablero.append(peon(i,"blanco"))
+        def peonNegro():
+            self.tablero.append(peon(i,"negro"))
+        def torreBlanca():
+            self.tablero.append(torre("blanco"))
+        def torreNegra():
+            self.tablero.append(torre("negro"))
+        def alfilBlanco():
+            self.tablero.append(alfil("blanco"))
+        def alfilNegro():
+            self.tablero.append(alfil("negro"))
+        def caballoBlanco():
+            self.tablero.append(caballo("blanco"))
+        def caballoNegro():
+            self.tablero.append(caballo("negro"))
+        def reinaBlanca():
+            self.tablero.append(reina("blanco"))
+        def reinaNegra():
+            self.tablero.append(reina("negro"))
+        def reyBlanco():
+            self.tablero.append(rey("blanco"))
+        def reyNegro():
+            self.tablero.append(rey("negro"))
         for i in tab:
-            switch(tab[i])
-                case '-1':   #Casilla blanca
-                    self.tablero.append(casilla("blanco"))
-                case '0':   #Casilla negra
-                    self.tablero.append(casilla("negro"))
-                case '1':   #Peon blanco
-                    self.tablero.append(peon(i,"blanco")
-                case '2':   #Torre blanca
-                    self.tablero.append(torre("blanco"))
-                case '3':   #Alfil blanco
-                    self.tablero.append(alfil("blanco"))
-                case '4':   #Caballo blanco
-                    self.tablero.append(caballo("blanco"))
-                case '5':   #Reina blanca
-                    self.tablero.append(reina("blanco"))
-                case '6':   #Rey blanco
-                    self.tablero.append(rey("blanco"))
-                case '7':   #Peon negro
-                    self.tablero.append(peon(i,"negro"))
-                case '8':   #Torre negra
-                    self.tablero.append(torre("negro"))
-                case '9':   #Alfil negro
-                    self.tablero.append(alfil("negro"))
-                case '10':   #Caballo negro
-                    self.tablero.append(caballo("negro"))
-                case '11':  #Reina negra
-                    self.tablero.append(reina("negro"))
-                case '12':
-                    self.tablero.append(rey("negro"))
+            options[tab[i]]
 
-m = Tablero()
+m = Tablero("tcarkactppppppppBbBbBbBbbBbBbBbBBbBbBbBbbBbBbBbBPPPPPPPPTCARKACT")
 for i in range (8):
     for j in range (8):
         print m.tablero[i*8 + j].tostring,
