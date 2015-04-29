beb94f1c28df438a3f4f09386da075dde232f5d9
jgaviri6 <jgavirias13@gmail.com>
2015-04-29 02:52:08 +0000

    Tablero ajedrez

--- tablero.py
+++ tablero.py
@@ -3,34 +3,48 @@
 empty_block = " "
 
 class Tablero(object):
+    tablero = []
     def __init__(self):
-
-        self.board = []
-        peones = "P " * 8
-        blancos = u'\u2656'
-        print blancos
-        peones_blancos = peones.strip()
-        negros = blancos.lower()
-        peones_negros = peones.lower()
-        self.board.append(negros.split(" "))
-        self.board.append(peones_negros.split(" "))
-        for i in range(4):
-            self.board.append([empty_block]*8)
-        self.board.append(peones_blancos.split(" "))
-        self.board.append(blancos.split(" "))
+        peonB = u'\u2659'
+        peonN = u'\u265F'
+        torreB = u'\u2656'
+        torreN = u'\u265C'
+        caballoB = u'\u2658'
+        caballoN = u'\u265E'
+        alfilB = u'\u2657'
+        alfilN = u'\u265D'
+        reyB = u'\u2654'
+        reyN = u'\u265A'
+        reinaB = u'\u2655'
+        reinaN = u'\u265B'
+        self.tablero.append(torreN)
+        self.tablero.append(caballoN)
+        self.tablero.append(alfilN)
+        self.tablero.append(reinaN)
+        self.tablero.append(reyN)
+        self.tablero.append(alfilN)
+        self.tablero.append(caballoN)
+        self.tablero.append(torreN)
+        for i in range(8):
+            self.tablero.append(peonN)
+        for i in range(32):
+            self.tablero.append(" ")
+        for i in range (8):
+            self.tablero.append(peonB)
+        self.tablero.append(torreB)
+        self.tablero.append(caballoB)
+        self.tablero.append(alfilB)
+        self.tablero.append(reinaB)
+        self.tablero.append(reyB)
+        self.tablero.append(alfilB)
+        self.tablero.append(caballoB)
+        self.tablero.append(torreB)
 
 columnas = "a b c d e f g h".split(" ")
 
-class Ver(object):
-    def __init__(self):
-        pass
-    def display(self, board):
-        print("%s: %s"%(" ", columnas))
-        print ("-"*50)
-        for i, row in enumerate(board):
-            row_marker = 8-i
-            print("%s: %s"%(row_marker, row))
 
 m = Tablero()
-v = Ver()
-v.display(m.board)
+for i in range (8):
+    for j in range (8):
+        print m.tablero[i*8 + j],
+    print
